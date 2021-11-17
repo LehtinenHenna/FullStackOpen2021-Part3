5a93038076398bc3ec345a4623c888375654d5fc
henna <henna.lehtinen91@gmail.com>
2021-11-17 12:12:03 +0200

    Did task 3.3: added page that shows one person's phonebook information according to id.

--- index.js
+++ index.js
@@ -27,10 +27,7 @@ let persons = [
 ]
 /*~~~~~~~~~~~~~~~~~~*/
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
+// api to get the info page
 app.get('/info', (request, response) => {
   const message = `Phonebook has info for ${persons.length} people`
   const date = new Date()
@@ -38,6 +35,24 @@ app.get('/info', (request, response) => {
     `<p>${message}</p>
     <p>${date}</p>`
   )
+})
+
+// api to get all persons
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+// api to get one person
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
 })
 /*~~~~~~~~~~~~~~~~~~*/
 const PORT = 3001
