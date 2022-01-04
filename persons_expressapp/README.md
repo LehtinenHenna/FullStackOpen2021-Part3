60148eb9d16da4c23d2aefc20f269bb4a2e52e4a
henna <henna.lehtinen91@gmail.com>
2022-01-04 11:46:21 +0200

    Did task 3.10: expressapp is now deployed in Heroku in address: https://shrouded-harbor-21993.herokuapp.com/api/persons

---
+++ persons_expressapp/README.md
@@ -0,0 +1,3 @@
+### A link to the heroku app
+
+https://shrouded-harbor-21993.herokuapp.com/api/persons
--- persons_expressapp/index.js
+++ persons_expressapp/index.js
@@ -130,8 +130,10 @@ app.post('/api/persons', (request, response) => {
 })
 
 /*~~~~~~~~~~~~~~~~~~*/
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
 
 // command to start backend dev server: npm run dev
