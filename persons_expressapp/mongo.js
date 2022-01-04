39bfd4f62e12661d02dabb2090ca3edf28e09042
henna <henna.lehtinen91@gmail.com>
2022-01-04 22:41:19 +0200

    Did task 3.12: Made a program mongo.js which allows you to add people to the phonebook database in MongoDB Atlas and view the data according to how many command line arguments are given.

---
+++ persons_expressapp/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url =
+  `mongodb+srv://phonebook_admin:${password}@cluster0.hz6iz.mongodb.net/person-app?retryWrites=true&w=majority`
+  
+mongoose.connect(url)
+
+// create the schema
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+// create the model based on the schema
+const Person = mongoose.model('Person', personSchema)
+
+
+// if name and number were given as command line arguments
+// e.g. node mongo.js <password> "Arto Hellas" 05246463666
+if (process.argv.length === 5) {
+  const newName = process.argv[3]
+  const newNumber = process.argv[4]
+
+  const person = new Person({
+    name: newName,
+    number: newNumber
+  })
+  person.save().then(result => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`)
+    mongoose.connection.close()
+  }) 
+}
+
+// if only password was given as command line argument
+// e.g. node mongo.js <password>
+else if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+else {
+  console.log('wrong number of arguments.')
+  mongoose.connection.close()  
+}
